dab3e3516fb6abe95b2696dc27e5c7afb25fc81a
waiwai2525 <waiwai.cps@gmail.com>
2023-08-05 14:38:44 +0900

    値を更新できるように変更

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,80 @@ import rocket from "./rocket.png";
 import "bulma/css/bulma.css";
 import { Box, Columns, Heading, Level, Image } from "react-bulma-components";
 
+const flightTimeToText = (flightTime?: number): string => {
+  if (flightTime == undefined) return "--.-";
+
+  // uint16_t の -1
+  if (flightTime == 65535) return "--.-";
+
+  return (flightTime / 1000.0).toFixed(1);
+};
+
+const getFlightMode = (flightModeNumber?: number): string | undefined => {
+  if (flightModeNumber == undefined) return undefined;
+  if (flightModeNumber < 0 || flightModeNumber > 9) return undefined;
+
+  return [
+    "SLEEP",
+    "STANDBY",
+    "THRUST",
+    "CLIMB",
+    "DESCENT",
+    "DECEL",
+    "PARACHUTE",
+    "LAND",
+    "SHUTDOWN",
+  ][flightModeNumber];
+};
+
+const getNorm = (x?: number, y?: number, z?: number): number | undefined => {
+  if (x == undefined || y == undefined || z == undefined) return undefined;
+  return Math.sqrt(x * x + y * y + z * z);
+};
+
+const degToDms = (deg?: number): string | undefined => {
+  if (deg == undefined) return undefined;
+
+  const d = Math.trunc(deg);
+  const ms = parseFloat("0." + deg.toString().split(".")[1]) * 60;
+  const m = Math.trunc(ms);
+  const s = parseFloat("0." + ms.toString().split(".")[1]) * 60;
+
+  return `${d.toFixed()}°${m.toFixed()}'${s.toFixed(2)}"`;
+};
+
 export const App = () => {
-  const [airData, setAirData] = useState<string | undefined>();
-  const [positionData, setPositionData] = useState<string | undefined>();
-  const [systemData, setSystemData] = useState<string | undefined>();
-  const [sensingData, setSensingData] = useState<string | undefined>();
-  const [powerData, setPowerData] = useState<string | undefined>();
-  const [valveData, setValveData] = useState<string | undefined>();
+  const [flightTime, setFlightTime] = useState<number | undefined>();
+  const [flightMode, setFlightMode] = useState<number | undefined>();
+  const [altitude, setAltitude] = useState<number | undefined>();
+  const [acceleration, setAcceleration] = useState<number | undefined>();
+  const [pitch, setPitch] = useState<number | undefined>();
+  const [latitude, setLatitude] = useState<number | undefined>();
+  const [longitude, setLongitude] = useState<number | undefined>();
 
   useEffect(() => {
     const socket = io(`http://${window.location.hostname}:3010`, {
       transports: ["websocket"],
     });
 
-    socket.on("air-data", (json) => setAirData(JSON.stringify(json)));
-    socket.on("position-data", (json) => setPositionData(JSON.stringify(json)));
-    socket.on("system-data", (json) => setSystemData(JSON.stringify(json)));
-    socket.on("sensing-data", (json) => setSensingData(JSON.stringify(json)));
-    socket.on("power-data", (json) => setPowerData(JSON.stringify(json)));
-    socket.on("valve-data", (json) => setValveData(JSON.stringify(json)));
+    socket.on("air-data", (json) => {
+      setAltitude(json.Alt);
+      setAcceleration(getNorm(json.Lia.x, json.Lia.y, json.Lia.z));
+      setPitch(json.Ori.y);
+    });
+    socket.on("position-data", (json) => {
+      setLatitude(json.Latitude);
+      setLongitude(json.Longitude);
+    });
+    socket.on("system-data", (json) => {
+      setFlightTime(json.FlightTime);
+      setFlightMode(json.FlightMode);
+    });
   }, []);
 
   return (
     <div style={{ position: "relative", width: "100%", height: "100vh" }}>
-      <GSI latitude={undefined} longitude={undefined} />
+      <GSI latitude={latitude} longitude={longitude} />
       <div
         style={{
           position: "absolute",
@@ -47,7 +97,9 @@ export const App = () => {
                 <Level>
                   <Level.Item>
                     <div>
-                      <Heading>X+ 10.0 sec</Heading>
+                      <Heading>
+                        {`X+ ${flightTimeToText(flightTime)} sec`}
+                      </Heading>
                     </div>
                   </Level.Item>
                 </Level>
@@ -57,7 +109,9 @@ export const App = () => {
                   <Level.Item>
                     <div>
                       <Heading size={6}>フライトモード</Heading>
-                      <Heading size={4}>STANDBY</Heading>
+                      <Heading size={4}>
+                        {getFlightMode(flightMode) ?? "-------"}
+                      </Heading>
                     </div>
                   </Level.Item>
                 </Level>
@@ -72,7 +126,9 @@ export const App = () => {
                     <Heading size={6} textAlign="center">
                       高度
                     </Heading>
-                    <Heading size={4}>100.0 m</Heading>
+                    <Heading size={4}>
+                      {`${altitude?.toFixed(2) ?? "---.--"} m`}
+                    </Heading>
                   </div>
                 </Level.Item>
                 <Level.Item>
@@ -81,11 +137,11 @@ export const App = () => {
                       加速度
                     </Heading>
                     <Heading size={4}>
-                      100.0 m/s
+                      {`${acceleration?.toFixed(2) ?? "---.--"} m/s`}
                       <span
                         style={{
                           verticalAlign: "super",
-                          fontSize: "0.75em",
+                          fontSize: "0.5em",
                         }}
                       >
                         2
@@ -105,9 +161,11 @@ export const App = () => {
                 }}
               >
                 <img
-                  width={120}
                   src={rocket}
-                  style={{ transform: "rotate(105deg)" }}
+                  style={{
+                    transform: `rotate(${-pitch + 90}deg)`,
+                    width: "120px",
+                  }}
                 ></img>
               </div>
             </Columns.Column>
@@ -120,7 +178,9 @@ export const App = () => {
                 <Level>
                   <Level.Item>
                     <div>
-                      <Heading size={5}>00°00"00.00' N</Heading>
+                      <Heading size={5}>{`${
+                        degToDms(latitude) ?? "--°--'--.--\""
+                      } N`}</Heading>
                     </div>
                   </Level.Item>
                 </Level>
@@ -129,7 +189,9 @@ export const App = () => {
                 <Level>
                   <Level.Item>
                     <div>
-                      <Heading size={5}>000°00"00.00' E</Heading>
+                      <Heading size={5}>{`${
+                        degToDms(longitude) ?? "---°--'--.--\""
+                      } E`}</Heading>
                     </div>
                   </Level.Item>
                 </Level>
